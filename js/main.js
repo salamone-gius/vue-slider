45e0d143d93622b51ad1839140450453796902a0
salamone-gius <salamone.gius@gmail.com>
2022-05-20 16:59:45 +0200

    click event added on thumbs

--- js/main.js
+++ js/main.js
@@ -63,13 +63,14 @@ const app = new Vue(
                     this.index--;
                 }
             },
-
+            // - imposto la funzione 'clickImage':
+            clickImage() {
+            },
         },
     },
 );
 
 
-// Bonus:
-// 1- al click su una thumb, visualizzare in grande l'immagine corrispondente
-// 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
-// 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
+// BONUS.1 Al click su una thumb, visualizzare in grande l'immagine corrispondente
+// BONUS.2 Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+// BONUS.3 Quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
