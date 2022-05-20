da9bcea013e0cfb5e1525146962f567cb100bc3c
salamone-gius <salamone.gius@gmail.com>
2022-05-20 17:40:34 +0200

    function clickImage created

--- js/main.js
+++ js/main.js
@@ -63,14 +63,16 @@ const app = new Vue(
                     this.index--;
                 }
             },
+            // BONUS.1 Al click su una thumb, visualizzare in grande l'immagine corrispondente:
             // - imposto la funzione 'clickImage':
-            clickImage() {
+            clickImage(imgIndex) {
+                // - al click, l'indice diventa uguale all'indice dell'immagine cliccata.
+                this.index = imgIndex;
             },
         },
     },
 );
 
 
-// BONUS.1 Al click su una thumb, visualizzare in grande l'immagine corrispondente
 // BONUS.2 Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
 // BONUS.3 Quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
